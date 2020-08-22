2dffc001aaac8d20e71bd0caa81c57fd3c748828
islamelhady <i.elhady44@gmail.com>
2020-08-22 19:52:53 +0200

    add fragment and replace

--- app/src/main/java/com/example/testfragment/MainActivity.java
+++ app/src/main/java/com/example/testfragment/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.testfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnX, btnY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnX = findViewById(R.id.main_btnX);
+        btnY = findViewById(R.id.main_btnY);
+
+        btnX.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                FragmentX fragmentX = new FragmentX();
+                ft.replace(R.id.main_fragment,fragmentX);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+
+        btnY.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                FragmentY fragmentY = new FragmentY();
+                ft.replace(R.id.main_fragment,fragmentY);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
     }
 }
