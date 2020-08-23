f842e2ca553bb2fbcde0dd2e3e8b8d6a9c9a3b85
islamelhady <i.elhady44@gmail.com>
2020-08-23 19:15:59 +0200

    send data from fragment to activity

--- app/src/main/java/com/example/testfragment/MainActivity.java
+++ app/src/main/java/com/example/testfragment/MainActivity.java
@@ -1,60 +1,26 @@
 package com.example.testfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NameFragment.OnFragmentClickListener {
+    TextView textView;
 
-    RecyclerView rv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.main_tv);
 
-        rv = findViewById(R.id.main_rv);
+    }
 
-        ArrayList<Name> names = new ArrayList<>();
-        names.add(new Name("islam"));
-        names.add(new Name("Mohamed"));
-        names.add(new Name("Jannat"));
-        names.add(new Name("Ali"));
-        names.add(new Name("ahmed"));
-        names.add(new Name("sarah"));
-        names.add(new Name("Mohamed"));
-        names.add(new Name("Jannat"));
-        names.add(new Name("Ali"));
-        names.add(new Name("ahmed"));
-        names.add(new Name("sarah"));
-
-        NameAdapter nameAdapter = new NameAdapter(names, new ItemOnClickListener() {
-            @Override
-            public void onClick(Name name) {
-
-                Bundle bundle = new Bundle();
-                bundle.putString("NAME",name.getName());
-
-                NameFragment nameFragment = new NameFragment();
-                nameFragment.setArguments(bundle);
-
-                FragmentManager fm = getSupportFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.replace(R.id.fragment_tv,nameFragment);
-                ft.commit();
-            }
-        });
-
-        rv.setAdapter(nameAdapter);
-        rv.setLayoutManager(new LinearLayoutManager(this));
-
+    @Override
+    public void onClickFragment(Name name) {
+        textView.setText(name.getName());
     }
 }
--- app/src/main/java/com/example/testfragment/NameFragment.java
+++ app/src/main/java/com/example/testfragment/NameFragment.java
@@ -1,15 +1,23 @@
 package com.example.testfragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 /**
@@ -19,18 +27,43 @@ public class NameFragment extends Fragment {
 
     String name;
     public static final String KEY_ARG = "NAME";
+    private OnFragmentClickListener listener;
 
     public NameFragment() {
         // Required empty public constructor
+    }
+
+    public static NameFragment newInstance(String name) {
+        Bundle bundle = new Bundle();
+        bundle.putString("NAME", name);
+         NameFragment nameFragment = new NameFragment();
+         nameFragment.setArguments(bundle);
+        return nameFragment;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentClickListener)
+        listener = (OnFragmentClickListener) context;
+        else
+            throw new ClassCastException("ur activity does not implements OnFragmentClickListener");
+
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bundle bundle = getArguments();
-        if (bundle != null){
+        if (bundle != null) {
             name = bundle.getString(KEY_ARG);
         }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
     }
 
     @Override
@@ -38,9 +71,36 @@ public class NameFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_name, container, false);
-        TextView textView = v.findViewById(R.id.fragment_tv_name);
-        textView.setText(name);
+        RecyclerView rv = v.findViewById(R.id.main_rv);
+
+        ArrayList<Name> names = new ArrayList<>();
+        names.add(new Name("islam"));
+        names.add(new Name("Mohamed"));
+        names.add(new Name("Jannat"));
+        names.add(new Name("Ali"));
+        names.add(new Name("ahmed"));
+        names.add(new Name("sarah"));
+        names.add(new Name("Mohamed"));
+        names.add(new Name("Jannat"));
+        names.add(new Name("Ali"));
+        names.add(new Name("ahmed"));
+        names.add(new Name("sarah"));
+
+        NameAdapter nameAdapter = new NameAdapter(names, new ItemOnClickListener() {
+            @Override
+            public void onClick(Name name) {
+                listener.onClickFragment(name);
+
+            }
+        });
+
+        rv.setAdapter(nameAdapter);
+        rv.setLayoutManager(new LinearLayoutManager(getActivity()));
         return v;
+    }
+
+    public interface OnFragmentClickListener{
+        void onClickFragment(Name name);
     }
 
 }
